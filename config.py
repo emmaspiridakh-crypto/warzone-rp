10816978bf4d27ed77fe9eb02e1cbcc970175b93
emma sp <emmaspiridakh@gmail.com>
2026-07-27 11:40:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -272,7 +272,7 @@ STATUS_BOTS_CHANNEL_ID    = 1525307053722304544  # PLACEHOLDER
 # GAME STATUS PANEL (Roblox)
 # =========================================================
 ROBLOX_UNIVERSE_ID = 8512095248  # PLACEHOLDER: universe id του Roblox game
-ROBLOX_GAME_URL = f"https://www.roblox.com/games/{ROBLOX_UNIVERSE_ID}"
+ROBLOX_GAME_URL = f"https://www.roblox.com/games/91226809988563/{ROBLOX_UNIVERSE_ID}"
 GAME_PANEL_BANNER_URL = "https://i.imgur.com/5O2AMGE.jpeg"  # PLACEHOLDER: banner εικόνα
 
 # =========================================================
